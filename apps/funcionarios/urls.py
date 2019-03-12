6132c7e4fa22c186ecde8f38f5e40fab23020cd0
Danilo <dasf_cm@terra.com.br>
2019-03-12 14:12:35 -0300

    iniciando o CRUD Funcionarios

--- apps/funcionarios/urls.py
+++ apps/funcionarios/urls.py
@@ -1,7 +1,7 @@
 from django.urls import path
-from .views import home
+from .views import FuncionariosList
 
 urlpatterns = [
-    path('', home),
+    path('', FuncionariosList.as_view(), name='list_funcionarios'),
 
 ]
--- apps/funcionarios/views.py
+++ apps/funcionarios/views.py
@@ -1,6 +1,10 @@
-from django.http import HttpResponse
-from django.shortcuts import render
+from django.views.generic import ListView
+from .models import Funcionario
 
 
-def home(request):
-    return HttpResponse('Olá')
+class FuncionariosList(ListView):
+    model = Funcionario
+
+    def get_queryset(self):
+        empresa_logada = self.request.user.funcionario.empresa
+        return Funcionario.objects.filter(empresa=empresa_logada)
