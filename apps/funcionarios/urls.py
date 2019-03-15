b52dcf45c30396959842656cd877cd79d215f1b1
Danilo <dasf_cm@terra.com.br>
2019-03-15 13:32:31 -0300

    CRUD funcionario, update and delete

--- apps/funcionarios/urls.py
+++ apps/funcionarios/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
-from .views import FuncionariosList
+from .views import FuncionariosList, FuncionarioEdit, FuncionarioDelete
 
 urlpatterns = [
     path('', FuncionariosList.as_view(), name='list_funcionarios'),
+    path('editar/<int:pk>/', FuncionarioEdit.as_view(), name='update_funcionario'),
+    path('delete/<int:pk>/', FuncionarioDelete.as_view(), name='delete_funcionario'),
 
 ]
--- apps/funcionarios/views.py
+++ apps/funcionarios/views.py
@@ -1,4 +1,5 @@
-from django.views.generic import ListView
+from django.urls import reverse_lazy
+from django.views.generic import ListView, UpdateView, DeleteView
 from .models import Funcionario
 
 
@@ -8,3 +9,13 @@ class FuncionariosList(ListView):
     def get_queryset(self):
         empresa_logada = self.request.user.funcionario.empresa
         return Funcionario.objects.filter(empresa=empresa_logada)
+
+
+class FuncionarioEdit(UpdateView):
+    model = Funcionario
+    fields = ['nome', 'departamentos']
+
+
+class FuncionarioDelete(DeleteView):
+    model = Funcionario
+    success_url = reverse_lazy('list_funcionarios')
