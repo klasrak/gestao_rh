03e0e4cc6f9732425cee410eb073c505a771ebf0
Danilo <dasf_cm@terra.com.br>
2019-03-20 15:56:05 -0300

    CRUD departamentos, create

--- apps/departamentos/views.py
+++ apps/departamentos/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-from django.views.generic import ListView
+from django.views.generic import ListView, CreateView
 from .models import Departamento
 
 
@@ -9,3 +9,14 @@ class DepartamentosList(ListView):
     def get_queryset(self):
         empresa_logada = self.request.user.funcionario.empresa
         return Departamento.objects.filter(empresa=empresa_logada)
+
+
+class DepartamentoCreate(CreateView):
+    model = Departamento
+    fields = ['nome']
+
+    def form_valid(self, form):
+        departamento = form.save(commit=False)
+        departamento.empresa = self.request.user.funcionario.empresa
+        departamento.save()
+        return super(DepartamentoCreate, self).form_valid(form)\
