c1a2b7298d00d2a5903d26894bb7d5fb3b73d234
Danilo <dasf_cm@terra.com.br>
2019-03-20 16:33:59 -0300

    requeriments.txt and django-bootstrap-forms

--- gestao_rh/settings.py
+++ gestao_rh/settings.py
@@ -19,6 +19,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'bootstrapform',
     'apps.empresas',
     'apps.funcionarios',
     'apps.departamentos',
