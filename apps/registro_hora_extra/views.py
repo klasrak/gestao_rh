356ce9dda2133443d99c9dca3570613d7613266e
Danilo <dasf_cm@terra.com.br>
2019-03-25 03:13:33 -0300

    CRUD registro hora extra, filtro select de funcionarios

--- apps/registro_hora_extra/views.py
+++ apps/registro_hora_extra/views.py
@@ -1,24 +1,28 @@
-from .models import RegistroHoraExtra
 from django.urls import reverse_lazy
+
+from .models import RegistroHoraExtra
+from .forms import RegistroHoraExtraForm
 from django.views.generic import (
     ListView,
     UpdateView,
     DeleteView,
     CreateView
 )
-
-
 class HoraExtraList(ListView):
     model = RegistroHoraExtra
-
     def get_queryset(self):
         empresa_logada = self.request.user.funcionario.empresa
         return RegistroHoraExtra.objects.filter(funcionario__empresa=empresa_logada)
 
-
 class HoraExtraEdit(UpdateView):
     model = RegistroHoraExtra
-    fields = ['motivo', 'funcionario', 'horas']
+    form_class = RegistroHoraExtraForm
+
+    def get_form_kwargs(self):
+        kwargs = super(HoraExtraEdit, self).get_form_kwargs()
+        kwargs.update({'user': self.request.user})
+        return kwargs
+
     success_url = reverse_lazy('list_hora_extra')
 
 
@@ -26,7 +30,13 @@ class HoraExtraDelete(DeleteView):
     model = RegistroHoraExtra
     success_url = reverse_lazy('list_hora_extra')
 
-
 class HoraExtraNovo(CreateView):
     model = RegistroHoraExtra
-    fields = ['motivo', 'funcionario', 'horas']
+    form_class = RegistroHoraExtraForm
+
+    def get_form_kwargs(self):
+        kwargs = super(HoraExtraNovo, self).get_form_kwargs()
+        kwargs.update({'user': self.request.user})
+        return kwargs
+
+    success_url = reverse_lazy('list_hora_extra')
