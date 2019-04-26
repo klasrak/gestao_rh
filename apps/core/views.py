859bc1514cf299c0ef5ab7922938b36376a7458d
Danilo <dasf_cm@terra.com.br>
2019-04-26 16:28:07 -0300

    iniciando API Django REST Framework

--- apps/core/views.py
+++ apps/core/views.py
@@ -1,5 +1,8 @@
 from django.shortcuts import render
 from django.contrib.auth.decorators import login_required
+from django.contrib.auth.models import User, Group
+from rest_framework import viewsets
+from apps.core.serializers import UserSerializer, GroupSerializer
 from apps.funcionarios.models import Funcionario
 
 
@@ -8,3 +11,15 @@ def home(request):
     data = {}
     data['usuario'] = request.user
     return render(request, 'core/index.html', data)
+
+
+class UserViewSet(viewsets.ModelViewSet):
+    queryset = User.objects.all().order_by('-date_joined')
+    serializer_class = UserSerializer
+
+
+class GroupViewSet(viewsets.ModelViewSet):
+    queryset = Group.objects.all()
+    serializer_class = GroupSerializer
+
+
