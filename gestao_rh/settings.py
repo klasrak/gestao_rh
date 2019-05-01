005efa333834f8e0bbf16b09ad9033605af0c943
Danilo <dasf_cm@terra.com.br>
2019-05-01 17:48:04 -0300

    configurando o celery na app

--- gestao_rh/settings.py
+++ gestao_rh/settings.py
@@ -22,6 +22,7 @@ INSTALLED_APPS = [
     'rest_framework',
     'rest_framework.authtoken',
     'bootstrapform',
+    'django_celery_results',
     'apps.empresas',
     'apps.funcionarios',
     'apps.departamentos',
@@ -119,3 +120,13 @@ STATIC_ROOT = os.path.join(BASE_DIR, "static/")
 LOGIN_REDIRECT_URL = 'home'
 
 LOGOUT_REDIRECT_URL = 'login'
+
+CELERY_RESULT_BACKEND = 'django.db'
+
+CELERY_BROKER_URL = 'redis://localhost:6379'
+
+CELERY_ACCEPT_CONTENT = ['application/json']
+
+CELERY_RESULT_SERIALIZER = 'json'
+
+CELERY_TASK_SERIALIZER = 'json'
